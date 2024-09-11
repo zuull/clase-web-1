f81c1dd3a5f2f5fd5545ae3ef1546109abf21362
zuull <zullyalejandra06@gmail.com>
2024-09-10 21:18:27 -0500

    let y const completos

--- trabajos jss/variables.js
+++ trabajos jss/variables.js
@@ -1,16 +1,36 @@
 //las variables  let 
 
-let nombre = "alejandra";
-let apellido 
+let nombre =(prompt ("pon tu nombre"));
+let apellido ;
+let numero= (prompt("escribe tu numero"));
+let edad ;
+let saludarNuevo = (" puedes entrar por ser mayor de edad")
 
 
-if (nombre == "alejandra"){
-    document.write("eres de apellido cardona");
+const nombreCorrecto = ("alejandra");
+const numAleja = (2);
+const numeroNuevo = (5)
+const edadAle = (20)
+const saludar = ("bienvenida aleja ")
+
+
+
+if (nombre === nombreCorrecto  ||  numero == numAleja){
+    apellido = ("eres de apellido cardona" + "<br>");
+    document.write (apellido);
+        if (nombre === nombreCorrecto  &&  edadAle == 20 ){
+            document.write(saludar + " y recuerda que tu ID es 2");
+            }
+    }
+
+else{ (nombre != "alejandra")
+let apellido =(prompt ("pon tu apellido "));
+document.write ("tu apellido es " + apellido + "<br>")
+let edad =(prompt ("pon tu edad"));
+    if (edad > 18){
+        document.write (saludarNuevo + "y rescuerda que tu ID es " + numero + "<br>")
+    } 
+document.write ("tu numero es " + numero)
 
 }
-else (nombre != "alejandra")
-apellido = "no sabemos tu apellido  ";
-document.write(apellido);
-
-prompt = "escribe tu nombre"
 

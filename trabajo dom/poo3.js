9974dd5ce83e3e10aa4f6b4f25f9f2771997147d
zuull <156028875+zuull@users.noreply.github.com>
2024-10-22 17:44:22 -0500

    -.

--- trabajo dom/poo3.js
+++ trabajo dom/poo3.js
@@ -1,30 +1,31 @@
-class Elemento {
-    constructor(idElemento) {
-        this.elemento = document.getElementById(idElemento);
-    }
+    // class Elemento {
+    //     constructor(idElemento) {
+    //         this.elemento = document.getElementById(idElemento);
+    //     }
 
-    cambiarColorFondo(color) {
-        this.elemento.style.backgroundColor = color;
-    }
-}
+    //     cambiarColorFondo(color) {
+    //         this.elemento.style.backgroundColor = color;
+    //     }
+    // }
 
-class Boton extends Elemento {
-    constructor(idElemento, idBoton) {
-        super(idElemento);
-        this.boton = document.getElementById(idBoton);
-        
-        // Añadir evento de clic al botón
-        this.boton.addEventListener('click', () => this.cambiarColor());
-    }
+    // class Boton extends Elemento {
+    //     constructor(idElemento, idBoton) {
+    //         super(idElemento);
+    //         this.boton = document.getElementById(idBoton);
+            
+    //         // Añadir evento de clic al botón
+    //         this.boton.addEventListener('click', () => this.cambiarColor());
+    //     }
 
-    cambiarColor() {
-        const colorAleatorio = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-        this.cambiarColorFondo(colorAleatorio);
-    }
-}
+    //     cambiarColor() {
+    //         const colorAleatorio = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    //         this.cambiarColorFondo(colorAleatorio);
+    //     }
+    // }
 
-// Inicializar las clases
-document.addEventListener('DOMContentLoaded', () => {
-    const elemento = new Elemento('mi-parrafo');
-    const boton = new Boton('mi-parrafo', 'btn-cambiar-color');
-});
+    // // Inicializar las clases
+    // document.addEventListener('DOMContentLoaded', () => {
+    //     const elemento = new Elemento('mi-parrafo');
+    //     const boton = new Boton('mi-parrafo', 'btn-cambiar-color');
+    // });
+console.log("hola mundo")

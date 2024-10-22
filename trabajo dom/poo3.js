18624301f988acedf6c807cf5829ef535b0d3447
zuull <156028875+zuull@users.noreply.github.com>
2024-10-22 17:41:01 -0500

    punto 3

---
+++ trabajo dom/poo3.js
@@ -0,0 +1,30 @@
+class Elemento {
+    constructor(idElemento) {
+        this.elemento = document.getElementById(idElemento);
+    }
+
+    cambiarColorFondo(color) {
+        this.elemento.style.backgroundColor = color;
+    }
+}
+
+class Boton extends Elemento {
+    constructor(idElemento, idBoton) {
+        super(idElemento);
+        this.boton = document.getElementById(idBoton);
+        
+        // Añadir evento de clic al botón
+        this.boton.addEventListener('click', () => this.cambiarColor());
+    }
+
+    cambiarColor() {
+        const colorAleatorio = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        this.cambiarColorFondo(colorAleatorio);
+    }
+}
+
+// Inicializar las clases
+document.addEventListener('DOMContentLoaded', () => {
+    const elemento = new Elemento('mi-parrafo');
+    const boton = new Boton('mi-parrafo', 'btn-cambiar-color');
+});
